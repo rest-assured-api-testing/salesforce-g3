1c8ae6ed96741f673249da99baef4e2451c4d029
Paola X. Aguilar Quiñones <paola.xaq@gmail.com>
2021-06-22 19:48:21 -0400

    adding all endpoints

--- src/main/java/api/ApiFeature.java
+++ src/main/java/api/ApiFeature.java
@@ -7,7 +7,13 @@ public enum ApiFeature {
     CONTACT("/sobjects/Contact"),
     CONTACT_ID("/sobjects/Contact/{contactId}"),
     ACCOUNT("/sobjects/Account"),
-    ACCOUNT_ID("/sobjects/Account/{accountId}");
+    ACCOUNT_ID("/sobjects/Account/{accountId}"),
+    PRODUCTS("/sobjects/Products2"),
+    PRODUCTS_ID("/sobjects/Products2/{productId}"),
+    CAMPAIGN("/sobjects/Campaign"),
+    CAMPAIGN_ID("/sobjects/Campaign/{campaignId}"),
+    PRICE_BOOK("/sobjects/PriceBook"),
+    PRICE_BOOK_ID("/sobjects/PriceBook/{priceId}");
 
     private String endpoint;
 
