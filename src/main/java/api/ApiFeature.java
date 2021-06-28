0a37f80e9c08c4d2a31ac733dc540d7f4d78ea1d
Jorge Caceres <jcaceresvelasco@gmail.com>
2021-06-28 18:01:44 -0400

    Add java doc for api package

--- src/main/java/api/ApiFeature.java
+++ src/main/java/api/ApiFeature.java
@@ -1,5 +1,18 @@
+/**
+ * Copyright (c) 2021 Fundacion Jala.
+ *
+ * This software is the confidential and proprietary information of Fundacion Jala
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Fundacion Jala
+ *
+ * @author Edson Anawaya Rios
+ */
 package api;
 
+/**
+ * This enum defines possible SalesForce Endpoints.
+ */
 public enum ApiFeature {
 
     TOKEN("/token"),
@@ -30,10 +43,14 @@ public enum ApiFeature {
 
     private String endpoint;
 
-    ApiFeature(String endpoint) {
+    ApiFeature(final String endpoint) {
         this.endpoint = endpoint;
     }
-
+    /**
+     * Gets the api end point command according to the Sales force feature.
+     *
+     * @return the end point.
+     */
     public String toEndpoint() {
         return endpoint;
     }
--- src/main/java/api/ApiResponse.java
+++ src/main/java/api/ApiResponse.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2021 Fundacion Jala.
+ *
+ * This software is the confidential and proprietary information of Fundacion Jala
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Fundacion Jala
+ *
+ * @author Edson Añawaya Rios
+ */
 package api;
 
 import io.restassured.response.Response;
@@ -6,41 +16,84 @@ import java.util.List;
 
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
+/**
+ * This class builds an api response.
+ */
 public class ApiResponse {
     private Response response;
 
-    public ApiResponse(Response response) {
+    public ApiResponse(final Response response) {
         this.response = response;
     }
 
     public ApiResponse() {
     }
 
+    /**
+     * Gets the api response.
+     *
+     * @return the response.
+     */
     public Response getResponse() {
         return response;
     }
 
-    public void setResponse(Response response) {
+    /**
+     * Sets the response.
+     *
+     * @param response to be set.
+     */
+    public void setResponse(final Response response) {
         this.response = response;
     }
 
+    /**
+     * Gets the api Status Code.
+     *
+     * @return the Status Code.
+     */
     public int getStatusCode() {
         return response.getStatusCode();
     }
 
-    public <T> T getBody(Class<T> cls) {
+    /**
+     * Gets the api body.
+     *
+     * @return the body.
+     * @param cls to ge the body.
+     * @param <T> class type to get the body.
+     */
+    public <T> T getBody(final Class<T> cls) {
         return response.getBody().as(cls);
     }
 
-    public <T> List getBodyList(Class<T> cls) {
+    /**
+     * Gets the api body list.
+     *
+     * @return the body list.
+     * @param cls to ge the body list.
+     * @param <T> class type to get the body list.
+     */
+    public <T> List getBodyList(final Class<T> cls) {
         return response.getBody().jsonPath().getList("", cls);
     }
 
-    public String getPath(String path) {
+    /**
+     * Gets the api path.
+     *
+     * @return the path.
+     * @param path of the value to be get.
+     */
+    public String getPath(final String path) {
         return response.getBody().jsonPath().getJsonObject(path);
     }
 
-    public void validateBodySchema(String schema) {
+    /**
+     * Validates a json Schema.
+     *
+     * @param schema of the value to be validated.
+     */
+    public void validateBodySchema(final String schema) {
         response.then().log().body().assertThat().body(matchesJsonSchemaInClasspath(schema));
     }
 }
