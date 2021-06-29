ed716bb8c84515a19cb19c90c69ae5a29bd7bd0c
edsonarios <edsonrios9@gmail.com>
2021-06-28 22:29:45 -0400

    add cucumber reports

--- src/test/java/rest/salesforce/org/runner/RunTests.java
+++ src/test/java/rest/salesforce/org/runner/RunTests.java
@@ -1,13 +1,15 @@
 package rest.salesforce.org.runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
-/*@CucumberOptions(
-        glue = {"rest.salesforce.org"},
-        features = {"src/test/resources/features"}
-)*/
+@CucumberOptions(
+        features = {"src/test/resources/features"},
+        plugin = {"html:target/site/cucumber-pretty.html", "json:target/cucumber.json"},
+        glue = {"rest.salesforce.org"}
+)
 public class RunTests extends AbstractTestNGCucumberTests {
     @BeforeTest
     public void beforeExecution() {
