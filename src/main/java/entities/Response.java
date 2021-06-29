edcd8051140c6328fefda3d5b76637e4d7d38e40
Jorge Caceres <jcaceresvelasco@gmail.com>
2021-06-29 14:36:27 -0400

    Add javadoc and checkstyle corrections to entities clasess

--- src/main/java/entities/Response.java
+++ src/main/java/entities/Response.java
@@ -1,33 +1,76 @@
+/**
+ * Copyright (c) 2021 Fundacion Jala.
+ *
+ * This software is the confidential and proprietary information of Fundacion Jala
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Fundacion Jala
+ *
+ * @author Edson Añawaya Rios
+ */
 package entities;
 
 import java.util.List;
 
+/**
+ * This class defines a created object response.
+ */
 public class Response {
     public String id;
     public boolean success;
     public List<Object> errors;
 
+    /**
+     * Gets the id.
+     *
+     * @return the id.
+     */
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
+    /**
+     * Sets the id.
+     *
+     * @param id to be set.
+     */
+    public void setId(final String id) {
         this.id = id;
     }
 
+    /**
+     * Gets the success.
+     *
+     * @return the success.
+     */
     public boolean isSuccess() {
         return success;
     }
 
-    public void setSuccess(boolean success) {
+    /**
+     * Sets the success.
+     *
+     * @param success to be set.
+     */
+    public void setSuccess(final boolean success) {
         this.success = success;
     }
 
+    /**
+     * Gets the errors.
+     *
+     * @return the errors.
+     */
     public List<Object> getErrors() {
         return errors;
     }
 
-    public void setErrors(List<Object> errors) {
+    /**
+     * Sets the errors.
+     *
+     * @param errors to be set.
+     */
+    public void setErrors(final List<Object> errors) {
         this.errors = errors;
     }
 }
